62bb3daf2c38905ac8d5df558a999385d308807d
Jasmeet Singh <42862193+encrptojs@users.noreply.github.com>
2024-05-21 15:22:56 +0530

    Models updated

--- shop/models.py
+++ shop/models.py
@@ -4,4 +4,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length= 50)
+    category = models.CharField(max_length=50,default="")
+    sub_category = models.CharField(max_length=50,default="")
+    price = models.IntegerField(default=0)
     pub_date = models.DateField()
+    description = models.CharField(max_length=300,default="Blank here comes the Description")
+    image = models.ImageField(upload_to="shop/images",default="")
+
+    def __str__(self):
+        return self.product_name
